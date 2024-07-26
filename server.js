17075f3b21e950a308c670a7b98059e3a20c2312
xunna <nnaemekauzuegbu59@gmail.com>
2024-07-26 18:09:59 +0100

    included cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { PORT } = require('./src/config/config');
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./src/routes/authRoutes');
 const categoryRoutes = require('./src/routes/categoryRoutes');
@@ -7,7 +8,7 @@ const questionRoutes = require('./src/routes/questionRoutes');
 const errorHandler = require('./src/middleware/errorHandler');
 const port = PORT;
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
